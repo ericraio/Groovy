22aa5c4b9e40a15335f9df08593498d17e1fea83
Eric Raio <ericraio@erics-air.gateway.2wire.net>
2014-01-03 18:29:20 -0800

    6.4 String Convenience Methods

---
+++ WorkingWithStrings/StringConvenience.groovy
@@ -0,0 +1,7 @@
+str = "It's a rainy day in Seattle"
+println str
+
+// -= operator is useful to manipulate a string, because it removes part
+// of the string that matches the string on the right side.
+str -= "rainy "
+println str
---
+++ WorkingWithStrings/StringRange.groovy
@@ -0,0 +1,8 @@
+// loops through each item in the range from the last character of the
+// first string (d) up to the last character of the last string (m)
+for(str in 'held'..'helm')
+{
+  print "${str} "
+}
+
+println ""
