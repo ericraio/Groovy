b664759e0bbf71e68345153438bcbd7e55ec7446
Eric Raio <ericraio@gmail.com>
2014-01-11 07:53:38 -0800

    10.2 Database Select

--- WorkingWithDatabases/Weather.groovy
+++ WorkingWithDatabases/Weather.groovy
@@ -1,3 +1,29 @@
 def sql = groovy.sql.Sql.newInstance('jdbc:mysql//localost:3306/weatherinfo', userid, password, 'com.mysql.jdbc.Driver')
 
 println sql.connection.catalog
+
+println "----"
+
+Println "City               Temperature"
+sql.eachRow('SELECT * from weather') {
+  printf "%-20s%s\n", it.city, it[1]
+}
+
+println "----"
+
+processMesa = { metaData ->
+  metaData.columnCount.times { i ->
+    printf "%-21s", metaData.getColumnLabel(i+1)
+  }
+
+  println "\n"
+}
+
+sql.eachRow('SELECT * from weather', processMeta) {
+  printf "%-20s %s\n", it.city, it[1]
+}
+
+println "----"
+
+rows = sql.rows('SELECT * from weather')
+println "Weather info available from ${rows.size()} cites"
